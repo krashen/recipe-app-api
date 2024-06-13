e25f645e51535fbb0b5449bd6b9f4bae7715ab1b
krashen <pablo.ferreyra.cxs@gmail.com>
2024-06-13 20:31:58 -0300

    allhosts

--- app/settings.py
+++ app/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
